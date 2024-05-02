67f9fef6de38e74187c1a9b00f0c3f5da90d5076
JungWon <minjw1229dev@gmail.com>
2024-05-03 08:46:27 +0900

    dfd

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -16,3 +16,23 @@ searchInputEl.addEventListener('blur', function() {
   searchInputEl.setAttribute('placeholder', '');
 });
 
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function() {
+  console.log(window.scrollY);
+  if(window.scrollY > 500) {
+    // 배지 숨기기
+    // gsap.to(요소, 지속시간(s), 옵션);
+    gsap.to(badgeEl, 0.6, {
+      opacity: 0,     // 숫자 값은 자연스러운 전환 효과 있음
+      display: 'none' // 문자 겂은 자연스러운 전환 효과는 없음 
+    });
+  } else {
+    // 배지 보이기
+    gsap.to(badgeEl, 0.6, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+}, 300/*ms*/))
+// _.throttle(함수, 시간(ms))
