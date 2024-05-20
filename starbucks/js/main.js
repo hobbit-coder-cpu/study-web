cacb7304479471135255485dfde5996d392ca5d6
JungWon <minjw1229dev@gmail.com>
2024-05-20 16:54:01 +0900

    dfdf

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -60,4 +60,28 @@ new Swiper('.promotion .swiper-container', {
   // autoplay: {
   //   delay: 5000
   // }
+  pagination: {
+    el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+    clickable: true // 사용자의 페이지 번호 요소 제어
+  },
+  navigation: {
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next'
+  }
 })
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function() {
+  isHidePromotion = !isHidePromotion;
+  console.log(isHidePromotion);
+  if(isHidePromotion){
+    // 숨김 처리!
+    promotionEl.classList.add('hide');
+  }else {
+    // 보임 처리!
+    promotionEl.classList.remove('hide');
+  }
+});
+
